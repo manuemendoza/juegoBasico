e975747eaed1a5d42de802a367500eb4536d92c6
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-10-15 00:43:40 +0200

    añadiendo js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*Inicio*/
+const transparencia = document.querySelector('.transparencia');
+const textoInicio = document.querySelector('.texto__inicio');
+const botonboton = document.querySelector('div .eliminar');
+const colorApagado = document.querySelector('.boton__boton');
+
+
+// function encender() {
+//     colorApagado.style.backgroundColor = ("greenyellow");
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio">¡Hola Pringados!</p>` }, 2000)
+//     setTimeout(function() { botonboton.style.display = "none"; }, 3000)
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio--estilo">El juego empieza en</p>` }, 3000)
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">3</p>` }, 4000)
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">2</p>` }, 5000)
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">1</p>` }, 6000)
+//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros"></p>` }, 7000)
+//     setTimeout(function() { transparencia.style.display = "none" }, 7000)
+// }
