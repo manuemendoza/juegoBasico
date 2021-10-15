5013887432c537d624c24ac1c17263cb594ea288
Manuel Mendoza <77455281+manuemendoza@users.noreply.github.com>
2021-10-15 12:32:58 +0200

    modificacion pantalla eleccion personaje

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ const botonboton = document.querySelector('div .eliminar');
 const colorApagado = document.querySelector('.boton__boton');
 
 
-// function encender() {
-//     colorApagado.style.backgroundColor = ("greenyellow");
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio">¡Hola Pringados!</p>` }, 2000)
-//     setTimeout(function() { botonboton.style.display = "none"; }, 3000)
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio--estilo">El juego empieza en</p>` }, 3000)
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">3</p>` }, 4000)
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">2</p>` }, 5000)
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">1</p>` }, 6000)
-//     setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros"></p>` }, 7000)
-//     setTimeout(function() { transparencia.style.display = "none" }, 7000)
-// }
+function encender() {
+    colorApagado.style.backgroundColor = ("greenyellow");
+    setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio">¡Hola Pringados!</p>` }, 2000)
+    setTimeout(function() { botonboton.style.display = "none"; }, 3000)
+    setTimeout(function() { textoInicio.innerHTML = `<p class="letras__inicio--estilo">El juego empieza en</p>` }, 3000)
+    setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">3</p>` }, 4000)
+    setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">2</p>` }, 5000)
+    setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros">1</p>` }, 6000)
+    setTimeout(function() { textoInicio.innerHTML = `<p class="inicio--estilo--numeros"></p>` }, 7000)
+    setTimeout(function() { transparencia.style.display = "none" }, 7000)
+}
+
+/*Eleccion de personajes*/
